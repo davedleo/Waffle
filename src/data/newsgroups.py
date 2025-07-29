cdf5f1c7bd5a2f903201a018fa561ad38b165f60
Davide Leo <davideleo@MacBook-Pro-di-Davide.local>
2025-07-30 00:28:22 +0200

    feat: add newspaper dataset

--- src/data/newsgroups.py
+++ src/data/newsgroups.py
@@ -1,9 +1,7 @@
 from os.path import join
 
-from PIL import Image
-
-from torch import Tensor
-from torchvision.transforms import Compose, ToTensor
+from numpy import float32, loadtxt
+from torch import Tensor, from_numpy 
 
 from typing import Optional, Dict, List, Any
 
@@ -14,8 +12,8 @@ from src.data.attacks import Attack
 
 
 
-TRAIN_DATA_PATH = join("data", "fashion_mnist", "train")
-TEST_DATA_PATH = join("data", "fashion_mnist", "test")
+TRAIN_DATA_PATH = join("data", "newsgroups", "train")
+TEST_DATA_PATH = join("data", "newsgroups", "test")
 
 
 
@@ -28,29 +26,25 @@ class Dataset(BaseDataset):
             self, 
             data: Dict[str, List[Any]],
             train: bool,
-            attack: Optional[Attack] = None
+            attack: Optional[Attack] = None # it is
     ): 
         super(Dataset, self).__init__(data)
-        loading_transforms = [ToTensor(), Normalize(.5, .5)] if attack is None else [ToTensor(), Normalize(.5, .5), attack]
-        self.loading_transforms = Compose(loading_transforms)
-        self.training_transforms = RandomAffine(degrees = 90, translate = (.1, .1), scale = (.9, 1.1)) if train else None
+        self.loading_transforms = attack
         return
     
 
     def __getitem__(self, index: int) -> list[Tensor, int]:
-        if self.training_transforms is not None: 
-            X, y = self.cache[index]
-            return self.training_transforms(X), y
-        else: 
-            return self.cache[index]
+        return self.cache[index]
     
 
     def load(self): 
-        for img_path, y, in zip(self.X, self.y):
-            x = Image.open(img_path).convert("L")
+        self.cache = []  
+        for txt_path, y in zip(self.X, self.y):
+            x_np = loadtxt(txt_path, dtype=float32)
+            x = from_numpy(x_np)
             x = self.loading_transforms(x)
             self.cache.append((x, y))
-        return 
+        return
     
 
 
